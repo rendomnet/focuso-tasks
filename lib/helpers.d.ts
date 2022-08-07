c81a756de52908a80127a4b1de3479944152ca07
Ruslan Zhunussov <rendom@gmail.com>
2022-08-07 12:04:42 +0400

    fixes

--- lib/helpers.d.ts
+++ lib/helpers.d.ts
@@ -1,7 +1,7 @@
 import { containerType } from "./types";
 export declare function setDeep(obj: object, keyPath: string | string[], value: any): void;
 export declare function isDate(value: any): boolean;
-export declare function getDate(value: any): Date;
+export declare function getDate(value: any): Date | null | undefined;
 export declare function getTimestamp(value: any): number;
 export declare function isEmptyContainer(item: containerType): boolean;
 export declare function findByLowesValue<T>(array: T[], propertyName: string): T | undefined;
