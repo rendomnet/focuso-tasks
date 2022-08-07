f3d8c96e59991c4f3979299f3ad9a7ddd8c47df7
Ruslan Zhunussov <rendom@gmail.com>
2022-08-07 12:42:06 +0400

    lib

---
+++ lib/helpers.d.ts
@@ -0,0 +1,8 @@
+import { containerType } from "./types";
+export declare function setDeep(obj: object, keyPath: string | string[], value: any): void;
+export declare function isDate(value: any): boolean;
+export declare function getDate(value: any): Date | null | undefined;
+export declare function getTimestamp(value: any): number;
+export declare function isEmptyContainer(item: containerType): boolean;
+export declare function findByLowesValue<T>(array: T[], propertyName: string): T | undefined;
+export declare function getContainerTasks(container: containerType): object;
